c69f470cda0aeee50ffc7255ace09d509eeb5824
Cristopher Barrios <cjrbarrios@gmail.com>
2019-04-05 22:59:43 -0600

    Final

--- app/src/main/java/com/example/lab_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_3/MainActivity.kt
@@ -6,11 +6,13 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    private var b: Button? = null
+
     private var textView: TextView? = null
+    private var textView2: TextView? = null
     private var editText: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,10 +20,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.textView) as TextView
+        textView2 = findViewById(R.id.textView2) as TextView
         editText = findViewById(R.id.editText) as EditText
     }
     fun onClic(v: View?){
-        textView?.visibility
-       editText?.visibility
+        textView?.visibility = View.GONE
+        editText?.visibility = View.GONE
     }
 }
